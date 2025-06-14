c2bf86fbbe1e6fbdea717d7979350a590d636c0c
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-14 17:52:52 +0100

    Add aiAssistant component with basic structure

---
+++ app/(tabs)/aiAssistant.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const aiAssistant = () => {
+  return (
+    <View>
+      <Text>aiAssistant</Text>
+    </View>
+  );
+};
+
+export default aiAssistant;
