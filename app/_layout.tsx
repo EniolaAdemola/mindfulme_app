45ac6117112fdaa3eff876ba56e8bfad6d14c24e
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-14 17:51:58 +0100

    Implement conditional rendering for authentication in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,43 @@
+// import { Stack } from "expo-router";
+// import "./globals.css";
+
+// export default function RootLayout() {
+//   return (
+//     <Stack>
+//       <Stack.Screen
+//         name="(tabs)"
+//         options={{
+//           headerShown: false,
+//         }}
+//       />
+//       <Stack.Screen
+//         name="articles/[id]"
+//         options={{
+//           headerShown: false,
+//         }}
+//       />
+//     </Stack>
+//   );
+// }
 import { Stack } from "expo-router";
+import React from "react";
+import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const isLoggedIn = false;
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {isLoggedIn ? (
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+      ) : (
+        <Stack.Screen name="(auth)" /> // Your onboarding & login stack
+      )}
+    </Stack>
+  );
 }
