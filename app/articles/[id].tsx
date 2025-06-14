1f956003629b975154df48b0b7ef3ae4c469044c
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-14 17:53:20 +0100

    Add ArticlesDetails component with basic structure

---
+++ app/articles/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ArticlesDetails = () => {
+  return (
+    <View>
+      <Text>ArticlesDetails</Text>
+    </View>
+  );
+};
+
+export default ArticlesDetails;
