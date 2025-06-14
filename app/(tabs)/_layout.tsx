4ec0ba98099390ba4a547be6c282953774a4e105
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-14 17:52:28 +0100

    Add layout component with tab navigation and icons

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,105 @@
+import { icons } from "@/constants/icons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+interface TabIconProps {
+  focused: boolean;
+  icon: any;
+  title: string;
+  isCenter?: boolean;
+}
+
+const ACTIVE = "#53389E";
+const INACTIVE = "#9ca3af";
+
+const TabIcon = ({ focused, icon, title }: TabIconProps) => {
+  const textColor = focused ? "text-[#53389E]" : "text-gray-400";
+  const textWeight = focused ? "font-bold" : "font-normal";
+
+  // Normal tab
+  return (
+    <View className="justify-center items-center w-20 flex-1 mt-2">
+      <Image
+        source={icon}
+        style={{
+          width: 20,
+          height: 30,
+          tintColor: focused ? ACTIVE : INACTIVE,
+        }}
+        resizeMode="contain"
+      />
+      <Text className={`text-xs ${textColor} ${textWeight}`}>{title}</Text>
+    </View>
+  );
+};
+
+export default function _layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          height: 80,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          position: "absolute",
+          borderTopWidth: 0,
+          elevation: 5,
+          paddingHorizontal: 8,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="analytics"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.Analytics}
+              title="Analytics"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="aiAssistant"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.AiChart}
+              title="AI Assistant"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="journal"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.Journal} title="Journal" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
