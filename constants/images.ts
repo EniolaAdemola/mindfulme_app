2989031bc871c91589b2efbdf7a09206069f9024
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-17 23:37:08 +0100

    Add additional image assets for enhanced visual content

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,23 @@
 import bg from "@/assets/images/bg.png";
 import highlight from "@/assets/images/highlight.png";
+import Image1 from "@/assets/images/image1.jpg";
+import Image2 from "@/assets/images/image2.jpg";
+import Image3 from "@/assets/images/image3.jpg";
+import Image4 from "@/assets/images/image4.jpg";
+import Image5 from "@/assets/images/image5.jpg";
+import Image6 from "@/assets/images/image6.png";
+import Image7 from "@/assets/images/image7.jpg";
 import rankingGradient from "@/assets/images/rankingGradient.png";
 
 export const images = {
   bg,
   highlight,
   rankingGradient,
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
 };
