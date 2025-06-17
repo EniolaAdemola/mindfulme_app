5ef144885ae4333e17b36e36ee79ff86d4dd8db6
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-17 23:37:48 +0100

    Refactor aiAssistant component to integrate AIChatScreen and enhance layout

--- app/(tabs)/aiAssistant.tsx
+++ app/(tabs)/aiAssistant.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import AIChatScreen from "@/components/AIChatScreen";
 import React from "react";
+import { ScrollView, View } from "react-native";
 
 const aiAssistant = () => {
   return (
-    <View>
-      <Text>aiAssistant</Text>
+    <View className="flex-1 bg-purple-100">
+      <ScrollView
+        // showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20 }}
+        className="flex-1"
+      >
+        <AIChatScreen />
+      </ScrollView>
     </View>
   );
 };
