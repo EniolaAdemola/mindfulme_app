9ec471b13aa75f54c78a62475c6b41732121024e
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-17 23:38:08 +0100

    Update icons import statements and export structure for consistency

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,8 +1,14 @@
 import AiChart from "@/assets/icons/AiChat.png";
+import arrowLeft from "@/assets/icons/arrowLeft.png";
+import articles from "@/assets/icons/article.png";
 import Analytics from "@/assets/icons/Chart.png";
-import Journal from "@/assets/icons/journal.png";
-import person from "@/assets/icons/person.png";
 import home from "@/assets/icons/home.png";
+import Journal from "@/assets/icons/journal.png";
+import notification from "@/assets/icons/notification.png";
+import message from "@/assets/icons/PaperPlaneRight-r.png";
+import person from "@/assets/icons/person.png";
+import Reports from "@/assets/icons/Reports.png";
+import search from "@/assets/icons/Search.png";
 
 export const icons = {
   home,
@@ -10,4 +16,10 @@ export const icons = {
   AiChart,
   Analytics,
   person,
+  notification,
+  search,
+  articles,
+  Reports,
+  arrowLeft,
+  message,
 };
