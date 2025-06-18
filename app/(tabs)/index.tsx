2343c96b4576af1c60e11ffd0f99fca336f20ac8
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-18 10:24:41 +0100

    Refactor Index component layout and styling for improved structure and readability

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import Categories from "@/components/Categories";
+import MoodCheck from "@/components/MoodCheck";
+import ProfileCard from "@/components/ProfileCard";
+import SearchBar from "@/components/SearchBar";
+import TodaysProgress from "@/components/TodaysProgress";
+import { ScrollView, View } from "react-native";
+import ArticleSection from "../articles/ArticleSection";
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 items-center justify-center"
-      style={{ flex: 1, backgroundColor: "#53389E" }}
-    >
-      <Text className="text-white text-5xl"> Mindfulme.</Text>
-      {/* <Link href="/articles/test" className="text-2xl">
-        Article
-      </Link> */}
+    <View className="flex-1 bg-purple-100">
+      {/* Purple Header Background */}
+
+      <ScrollView
+        // showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20 }}
+        className="flex-1"
+      >
+        <View className="px-5 py-12  bg-[#53389E]">
+          <ProfileCard />
+        </View>
+
+        <View className="px-4 mt-3 ">
+          <SearchBar />
+        </View>
+
+        <View className="px-4 mt-2 ">
+          <MoodCheck />
+        </View>
+
+        <View className="px-4 mt-2 ">
+          <Categories />
+        </View>
+
+        <View className="px-4 mt-2 ">
+          <TodaysProgress />
+        </View>
+
+        <View className="px-5 mt-6 mb-24">
+          <ArticleSection />
+        </View>
+
+        <View className="px-5 mt-6 mb-24">{/* <Onboarding /> */}</View>
+      </ScrollView>
     </View>
   );
 }
